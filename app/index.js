84c367abf0b1ee21d1e787e484e5ff28ecf7c137
Sean <seangores@gmail.com>
2014-02-02 02:13:19 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,155 @@
+"use strict";
+var util = require("util");
+var path = require("path");
+var yeoman = require("yeoman-generator");
+
+function getTestFrameworkOptions(options) {
+// setup the test-framework property, Gruntfile template will need this
+	this.testFramework = options["test-framework"] || "mocha";
+	this.coffee = options.coffee;
+
+	// for hooks to resolve on mocha by default
+	options["test-framework"] = this.testFramework;
+
+	// resolved to mocha by default (could be switched to jasmine for instance)
+	this.hookFor("test-framework", {
+		as:      "app",
+		options: {
+			options: {
+				"skip-install": options["skip-install-message"],
+				"skip-message": options["skip-install"]
+			}
+		}
+	});
+
+	this.options = options;
+}
+var Html5AppGenerator = module.exports = function Html5AppGenerator(args, options, config) {
+	yeoman.generators.Base.apply(this, arguments);
+
+	//getTestFrameworkOptions.call(this, options);
+	this.on("end", function () {
+		this.installDependencies({ skipInstall: options["skip-install"] });
+	});
+
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, "../package.json")));
+};
+
+util.inherits(Html5AppGenerator, yeoman.generators.Base);
+
+function getCurrentDate()
+{
+	return new Date().getUTCDate();
+}
+
+Html5AppGenerator.prototype.askFor = function askFor(_) {
+	var cb = this.async();
+
+	// have Yeoman greet the user.
+	console.log(this.yeoman);
+
+	var prompts = [
+		{
+			name:    "appname",
+			message: "What's the name of your app?",
+			default: this._.slugify(this.appname)
+		},
+		{
+			type: "confirm",
+			name: "coffee",
+			message: "Use coffeescript?"
+		},
+		{
+			type: "confirm",
+			name: "includeModernizr",
+			message: "Use Modernizr?"
+		},
+		{
+			type: "confirm",
+			name: "includejQuery",
+			message: "Use jQuery?"
+		},
+		{
+			name: "description",
+			message: "Describe your component",
+			default: "A sample description"
+		},
+		{
+			name: "authorName",
+			message: "What\"s your name?",
+			default: "Author name"
+		},
+		{
+			name: "authorEmail",
+			message: "What\"s your e-mail?",
+			default: "email@somedomain.com"
+		},
+		{
+			name: "authorURL",
+			message: "What\"s your website?",
+			default: "somedomain.com"
+		},
+		{
+			type: "confirm",
+			name: "includeCompass",
+			message: "Use compass (scss)?"
+		},
+		{
+			type: "confirm",
+			name: "coffee",
+			message: "Use coffeescript?"
+		},
+		{
+			name: "license",
+			message: "What's the copyright license?",
+			default: "MIT"
+		}
+	];
+
+	this.prompt(prompts, function (props) {
+		this.description = props.description;
+		this.appname = props.appname;
+		this.authorName = props.authorName;
+		this.authorEmail = props.authorEmail;
+		this.authorURL = props.authorURL;
+		this.license = props.license;
+		this.includeModernizr = props.includeModernizr;
+		this.coffee = props.coffee;
+		this.includejQuery = props.includejQuery;
+		this.includeCompass = props.includeCompass;
+		this.testFramework = props.testFramework;
+		this.currentDate = getCurrentDate();
+		cb();
+	}.bind(this));
+};
+
+Html5AppGenerator.prototype.app = function app() {
+	this.mkdir("app");
+	this.mkdir("app/templates");
+
+	this.template("_package.json", "package.json");
+	this.template("_bower.json", "bower.json");
+};
+
+Html5AppGenerator.prototype.projectfiles = function projectfiles() {
+	this.copy("editorconfig", ".editorconfig");
+	this.copy("jshintrc", ".jshintrc");
+	this.copy("_gitignore", ".gitignore");
+	this.copy("htaccess", ".htaccess");
+	this.copy("gitattributes", ".gitattributes");
+	this.copy("favicon.ico", "favicon.ico");
+};
+
+Html5AppGenerator.prototype.createEmptyDirectories = function createEmptyDirectories() {
+	this.mkdir("app/scripts");
+	this.mkdir("app/styles");
+	this.mkdir("app/img");
+};
+
+Html5AppGenerator.prototype.generateHTML = function generateHTML() {
+	this.template("_index.html", "index.html");
+};
+
+Html5AppGenerator.prototype.scaffoldGruntFile = function scaffoldGruntFile() {
+	this.template("_gruntfile.js", "Gruntfile.js");
+};
---
+++ test/test-creation.js
@@ -0,0 +1,74 @@
+/*global describe, beforeEach, it*/
+"use strict";
+
+/*
+TODO: Fix name not showing up in bower
+TODO: Scaffold out unit tests folder
+TODO: Scaffold out sample unit tests
+TODO: Scaffold out sample main.scss
+TODO: Scaffold out scripts, whether coffeescript of js
+*/
+
+var path = require("path");
+var helpers = require("yeoman-generator").test;
+var defaultOptions = {
+	"appname": this.appName,
+	"description": "Sample description",
+	"author":  {
+		"name": "Sean Goresht",
+		"email": "seangores@gmail.com",
+		"url": "http://seangoresht.com"
+	}
+};
+
+describe("html5-app generator", function () {
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, "temp"), function (err) {
+			if (err) {
+				return done(err);
+			}
+
+			this.app = helpers.createGenerator("html5-app:app", [
+				"../../app"
+			]);
+			done();
+		}.bind(this));
+	});
+
+	it("creates expected files", function (done) {
+		var expected = [
+			// add files you expect to exist here.
+			".jshintrc",
+			".editorconfig"
+		];
+
+		helpers.mockPrompt(this.app, defaultOptions);
+		this.app.options["skip-install"] = true;
+		this.app.run({}, function () {
+			helpers.assertFiles(expected);
+			done();
+		});
+	});
+	it("sets the name as default as the folder name", function () {
+		// arrange
+		var testFileName = "my-directory";
+		// act
+		// assert
+	});
+	it("creates empty directories", function () {
+		// Arrange
+		var emptyFolderList = [
+			"img",
+			"styles",
+			"scripts"
+		];
+		// Act
+		helpers.mockPrompt(this.app, defaultOptions);
+		this.app.options["skip-install"] = true;
+		// Assert
+		this.app.run({}, function () {
+			helpers.assertFiles(emptyFolderList);
+			done();
+		});
+	});
+});
